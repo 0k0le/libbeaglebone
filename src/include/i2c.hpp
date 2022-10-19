7bf98af061071285faafbd27fb22749e46475ef2
Matthew Geiger <mgeiger@newtonlabs.com>
2022-10-19 10:31:28 -0700

    Attempting to fix with forced C linkage

--- src/include/i2c.hpp
+++ src/include/i2c.hpp
@@ -17,7 +17,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
-#include <i2c/smbus.h>
+//#include <i2c/smbus.h>
 
 #define DEFAULT_BBG_I2C_DEVICE 1
 #define I2C_DEVICE_PREFIX "/dev/i2c-"
@@ -27,6 +27,8 @@ typedef struct i2cdevice {
 	int addr;
 	int index;
 } i2cdevice;
+
+extern "C" __s32 i2c_smbus_read_byte(int file);
 
 BBG_err i2c_open_device(i2cdevice *i2cdev, int device_index = DEFAULT_BBG_I2C_DEVICE);
 BBG_err i2c_close_device(i2cdevice *i2cdev);
